9c9b8ce6e697e3218c4be414ac628e8aed2c8816
canonical <canonical_entropy@163.com>
2024-05-02 21:32:37 +0800

    目前必须引入janino依赖。后续会修改为可选依赖

--- nop-solon/nop-solon-service-demo/src/main/java/io/nop/solon/demo/SolonDemoMain.java
+++ nop-solon/nop-solon-service-demo/src/main/java/io/nop/solon/demo/SolonDemoMain.java
@@ -3,7 +3,6 @@ package io.nop.solon.demo;
 import org.noear.solon.Solon;
 import org.noear.solon.annotation.Import;
 
-@Import(scanPackages = "io.nop.solon")
 public class SolonDemoMain {
     public static void main(String[] args) {
         Solon.start(SolonDemoMain.class, args);
