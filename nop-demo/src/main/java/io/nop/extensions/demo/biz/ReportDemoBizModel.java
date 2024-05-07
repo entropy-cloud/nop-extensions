31ca994e822de0b39a5dc782150428424226516e
flytreeleft <flytreeleft@crazydan.org>
2024-05-07 22:24:28 +0800

    调整 Maven 项目组织结构，为本项目设置父模块 io.github.entropy-cloud:nop-extensions 以便于做整体构建并新增其他扩展模块；将 ReportDemoBizModel 提取到独立的 nop-demo 子模块中，并以依赖形式引入到 nop-solon 等模块的演示中，从而实现代码复用，避免重复 copy 文件；

--- nop-demo/src/main/java/io/nop/extensions/demo/biz/ReportDemoBizModel.java
+++ nop-demo/src/main/java/io/nop/extensions/demo/biz/ReportDemoBizModel.java
@@ -5,7 +5,7 @@
  * Gitee:  https://gitee.com/canonical-entropy/nop-entropy
  * Github: https://github.com/entropy-cloud/nop-entropy
  */
-package io.nop.solon.demo.biz;
+package io.nop.extensions.demo.biz;
 
 import io.nop.api.core.annotations.biz.BizModel;
 import io.nop.api.core.annotations.biz.BizQuery;
