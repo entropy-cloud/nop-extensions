2c325d640ba8faa9bca71b18328b44280556063a
flytreeleft <flytreeleft@crazydan.org>
2024-05-08 11:09:28 +0800

    将 nop-solon-service-demo 中的启动类 SolonDemoMain 更名为 SolonServiceDemoMain 以避免与 nop-solon-demo 中的启动类同名而造成本地开发的名称冲突麻烦

---
+++ nop-solon/nop-solon-service-demo/src/main/java/io/nop/solon/demo/SolonServiceDemoMain.java
@@ -0,0 +1,9 @@
+package io.nop.solon.demo;
+
+import org.noear.solon.Solon;
+
+public class SolonServiceDemoMain {
+    public static void main(String[] args) {
+        Solon.start(SolonServiceDemoMain.class, args);
+    }
+}
